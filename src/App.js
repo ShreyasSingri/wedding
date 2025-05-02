68a2e227ca413e62789d325e6d54ef369f8cb82d
ShreyasSingri <sm.shreyasstar@gmail.com>
2025-05-02 23:02:59 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -7,31 +7,32 @@ function App() {
   return (
     <div className="App">
       <br />
-      <img src={require('./1.png')} alt="Wedding Invitation" style={{height:"150px"}} />
-      <h1 style={{marginTop:"0px"}}>Shreyas Weds Sankalpa</h1>
-      <h1>Save the date:</h1>
+      <img src={require('./1.png')} alt="Wedding Invitation" style={{ height: "150px" }} />
+      <h1>Shreyas Weds Sankalpa</h1>
+      <h2>Save the date:</h2>
       <div style={{ display: 'flex', justifyContent: 'space-around' }}>
         <div>
-          <h2>Reception:</h2>
-          <h3>14th May 2025</h3>
-          <h3>7:00 PM onwards </h3>
+          <h3>Reception:</h3>
+          <h4>14th May 2025</h4>
+          <h4>7:00 PM onwards </h4>
         </div>
         <div>
-          <h2>Muhrutham:</h2>
-          <h3>15th May 2025</h3>
-          <h3>10:30 AM onwards </h3>
+          <h3>Muhrutham:</h3>
+          <h4>15th May 2025</h4>
+          <h4>10:30 AM onwards </h4>
         </div>
       </div>
+      <br />
       {/* <iframe src="https://drive.google.com/file/d/10HXhe3UGZRlcp2yO5J8wXQ_x-BC7EjYj/preview"loading="lazy" width="640" height="360" allow="autoplay" title="Wedding Invitation Video"></iframe> */}
       <div className="video-container">
-      <iframe
-        src="https://drive.google.com/file/d/1OYCPWgxFChXv5Ds6rixtK6oFOX7GqPaV/preview"
-        title="Google Drive Video"
-        className="video-iframe"
-        allow="autoplay"
-        frameBorder="0"
-      ></iframe>
-    </div>
+        <iframe
+          src="https://drive.google.com/file/d/1OYCPWgxFChXv5Ds6rixtK6oFOX7GqPaV/preview"
+          title="Google Drive Video"
+          className="video-iframe"
+          allow="autoplay"
+          frameBorder="0"
+        ></iframe>
+      </div>
       <br />
       <br />
       <FlipClockCountdown
@@ -46,7 +47,7 @@ function App() {
       />
       <h2>Venue:</h2>
       <h3>Vasavi Convention Hall</h3>
-      <p>4, Vanivilas Rd, Beside to Vasavi Vidyanikethan High School,<br/> Vishweshwarapura, Basavanagudi, Bengaluru, Karnataka 560004</p>
+      <p>4, Vanivilas Rd, Beside to Vasavi Vidyanikethan High School, Vishweshwarapura, Basavanagudi, Bengaluru, Karnataka 560004</p>
       <br />
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.3648916621173!2d77.573611274841!3d12.948487087364704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae15ec317ff977%3A0x8c8ae92292729817!2sVasavi%20Convention%20Center!5e0!3m2!1sen!2sin!4v1743265812525!5m2!1sen!2sin"
         title='Google Maps'
@@ -54,35 +55,17 @@ function App() {
         allowfullscreen=""
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"></iframe>
-        <br />
-        <br />
-        <h2>Awaiting your presence<br />Join us to make our day Special & Memoreable!!!</h2>
-        <br />
-        <h2>Check out more .....</h2>
-        <div className="video-container">
-      <iframe
-        src="https://drive.google.com/file/d/1PBRAZnXO0YW5whaO-MaQPgL30mSg4fMH/preview"
-        title="Google Drive Video"
-        className="video-iframe"
-        allow="autoplay"
-        frameBorder="0"
-      ></iframe>
-    </div>
-        <br />
-        <br />
-        <div className="video-container">
-      <iframe
-        src="https://drive.google.com/file/d/1P3IdDC-9sSSGsx_swAJ0epv4SKUG2NsP/preview"
-        title="Google Drive Video"
-        className="video-iframe"
-        allow="autoplay"
-        frameBorder="0"
-      ></iframe>
-    </div>
-        <br />
-        <br />
-        <br />
-        <br />
+      <br />
+      <br />
+      <h2>Awaiting your presence<br />Join us to make our day Special & Memoreable!!!</h2>
+      <br />
+      <h3>With love from<br />Shreyas & Sankalpa</h3>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
